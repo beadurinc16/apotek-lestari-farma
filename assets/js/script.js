aa43f39b7b1fe10ce5bfcd3dc97ba992aef5a2ad
beadurinc16 <indrasandman@gmail.com>
2025-08-27 13:47:42 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,11 @@
 const animateElements = document.querySelectorAll('.animate');
 
 const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if(entry.isIntersecting){
-            entry.target.classList.add('visible');
-        }
-    });
-},{ threshold: 0.2 });
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('visible');
+    }
+  });
+}, { threshold: 0.2 });
 
 animateElements.forEach(el => observer.observe(el));
